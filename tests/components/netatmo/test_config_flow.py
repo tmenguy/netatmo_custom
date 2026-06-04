b69618ffaa1aae333deffa9d01bb8a7344ebe2f0
tmenguy <thomas.menguy@gmail.com>
2026-06-04 15:22:45 +0200

    HA update

--- tests/components/netatmo/test_config_flow.py
+++ tests/components/netatmo/test_config_flow.py
@@ -44,13 +44,13 @@ async def test_abort_if_existing_entry(hass: HomeAssistant) -> None:
     flow.hass = hass
 
     result = await hass.config_entries.flow.async_init(
-        "netatmo", context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "single_instance_allowed"
 
     result = await hass.config_entries.flow.async_init(
-        "netatmo",
+        DOMAIN,
         context={"source": config_entries.SOURCE_HOMEKIT},
         data=ZeroconfServiceInfo(
             ip_address=ip_address("192.168.1.5"),
@@ -75,7 +75,7 @@ async def test_full_flow(
     """Check full flow."""
 
     result = await hass.config_entries.flow.async_init(
-        "netatmo", context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
     state = config_entry_oauth2_flow._encode_jwt(
         hass,
@@ -240,7 +240,7 @@ async def test_reauth(
     """Test initialization of the reauth flow."""
 
     result = await hass.config_entries.flow.async_init(
-        "netatmo", context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
     state = config_entry_oauth2_flow._encode_jwt(
         hass,
